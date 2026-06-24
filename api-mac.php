dc81c16b33f17ddc3b921d91b830149588c5fb01
rosioleydicondori-create <rosioleydicondori@gmail.com>
2026-06-24 15:41:28 +0530

    Update api-mac.php

--- api-mac.php
+++ api-mac.php
@@ -13,29 +13,7 @@
   cursor: none;
 }
 </style>
-<script>
-let escTimer = null;
-let escHeld = false;
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && !escHeld) {
-        escHeld = true;
-
-        escTimer = setTimeout(() => {
-            if (document.fullscreenElement) {
-                document.exitFullscreen();
-            }
-        }, 4000); // 4 seconds
-    }
-});
-
-document.addEventListener('keyup', (e) => {
-    if (e.key === 'Escape') {
-        escHeld = false;
-        clearTimeout(escTimer);
-    }
-});
-</script>
 </head>
 <body>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
@@ -46,7 +24,7 @@ document.addEventListener('keyup', (e) => {
   allowfullscreen 
   webkitallowfullscreen 
   mozallowfullscreen 
-  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *" style="z-index:9999; position:fixed; inset:0; pointer-events:auto; overflow:hidden;">
+  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *" >
 </iframe>
 </body>
 </html>
